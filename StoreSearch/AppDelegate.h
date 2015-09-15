2f5308aa138d68f68e58d502cdf5a0852ecada17
Ekaterina <ekaterina@Ekaterinas-MacBook-Pro.local>
2015-09-15 14:04:05 +0300

    Initial Commit

---
+++ StoreSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by Ekaterina on 9/15/15.
+//  Copyright (c) 2015 Ekaterina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StoreSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoreSearch
+//
+//  Created by Ekaterina on 9/15/15.
+//  Copyright (c) 2015 Ekaterina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
