d84191a0299bc35eb179f8eba3611e1b8ff5eb91
Ekaterina <ekaterina@Ekaterinas-MacBook-Pro.local>
2015-09-17 17:55:15 +0300

    moved the logic for the "kind" method in SearchResultCell, added functionality for showing the pictures in the imageView.

--- StoreSearch/SearchViewController.m
+++ StoreSearch/SearchViewController.m
@@ -95,47 +95,12 @@ static NSString *const LoadingCellIdentifier = @"LoadingCell";
    SearchResultCell *cell = (SearchResultCell *)[tableView dequeueReusableCellWithIdentifier:SearchResultCellIdentifier forIndexPath:indexPath];
  
    SearchResult *searchResult = _searchResults[indexPath.row];
-        cell.nameLabel.text = searchResult.name;
+        [cell configureForSearchResult:searchResult];
         
-        NSString *artistName = searchResult.artistName;
-        
-        if (artistName == nil) {
-            artistName = @"Unknown";
-        }
-        
-        NSString *kind = [self kindForDisplay:searchResult.kind];
-        cell.artistNameLabel.text = [NSString stringWithFormat:@"%@ (%@)", artistName, kind];
-   
     return cell;
     }
 }
 
-- (NSString *)kindForDisplay:(NSString *)kind {
-    
-    if ([kind isEqualToString:@"album"]) {
-        return @"Album";
-    } else if ([kind isEqualToString:@"audiobook"]) {
-        return @"Audio Book";
-    } else if ([kind isEqualToString:@"book"]) {
-        return @"Book";
-    } else if ([kind isEqualToString:@"ebook"]) {
-        return @"E-Book";
-    } else if ([kind isEqualToString:@"feature-movie"]) {
-        return @"Movie";
-    } else if  ([kind isEqualToString:@"music-video"]){
-        return @"Music Video";
-    } else if ([kind isEqualToString:@"podcast"]) {
-        return @"Podcast";
-    } else if ([kind isEqualToString: @"software"]) {
-        return @"Software";
-    } else if ([kind isEqualToString:@"song"]) {
-        return @"Song";
-    } else if ([kind isEqualToString:@"tv-episode"]) {
-        return @"TV Episode";
-    } else {
-        return kind;
-    }
-}
 #pragma mark - UISearchBarDelegate
 - (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
     
